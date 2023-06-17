2b2a6892c856be1175bc003d483b6151f78dd8e7
David Gerson <84614495+pogrammerX@users.noreply.github.com>
2023-06-17 23:15:03 +0200

    Added 1.0

---
+++ AtomDB/Atom.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomDB
+{
+    /// <summary>
+    /// Atom
+    /// </summary>
+    public class Atom
+    {
+        Dictionary<byte[], byte[]> database = new();
+
+        internal Atom() { }
+
+        public static Atom Create()
+        {
+            if (!AtomManager.Initialized) throw new("Atom not initialized, please call AtomManager.Initialize() before using any Atom functions!");
+            return new()
+            {
+                database = new()
+            };
+        }
+
+        public static Atom Load(string path)
+        {
+            if (!AtomManager.Initialized) throw new("Atom not initialized, please call AtomManager.Initialize() before using any Atom functions!");
+            MemoryStream stream = new MemoryStream(File.ReadAllBytes(path));
+            BinaryReader br = new BinaryReader(stream);
+            ulong count = br.ReadUInt64();
+            Dictionary<byte[], byte[]> db = new();
+            for (ulong i = 0; i < count; i++)
+            {
+                var keyLength = br.ReadInt32();
+                byte[] keyBytes = br.ReadBytes(keyLength);
+
+                var valueLength = br.ReadInt32();
+                byte[] valueBytes = br.ReadBytes(valueLength);
+
+                db.Add(keyBytes, valueBytes);
+            }
+            br.Dispose();
+            stream.Dispose();
+            return new()
+            {
+                database = db
+            };
+        }
+
+        public byte[] GetBytes(byte[] key)
+        {
+            if (!database.ContainsKey(key)) throw new("Key was not found.");
+            return database[key];
+        }
+
+        public byte[] GetBytes(string key)
+        {
+            return GetBytes(Encoding.UTF8.GetBytes(key));
+        }
+
+        public void SetBytes(string key, byte[] value)
+        {
+            if (database.ContainsKey(Encoding.UTF8.GetBytes(key)))
+                database[Encoding.UTF8.GetBytes((string)key)] = value;
+            else
+                database.Add(Encoding.UTF8.GetBytes((string)key), value);
+        }
+
+        public void SetBytes(byte[] key, byte[] value)
+        {
+            if (database.ContainsKey(key))
+                database[key] = value;
+            else
+                database.Add(key, value);
+        }
+
+        public void SetInt(string key, int value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetInt(byte[] key, int value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetUInt(string key, uint value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetUInt(byte[] key, uint value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetULong(string key, ulong value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetULong(byte[] key, ulong value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetLong(string key, long value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetLong(byte[] key, long value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetShort(string key, short value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetShort(byte[] key, short value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetUShort(string key, ushort value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetUShort(byte[] key, ushort value)
+        {
+            SetBytes(key, BitConverter.GetBytes(value));
+        }
+
+        public void SetString(string key, string value)
+        {
+            SetString(Encoding.UTF8.GetBytes(key), value);
+        }
+
+        public void SetString(byte[] key, string value)
+        {
+            MemoryStream ms = new();
+            BinaryWriter bw = new(ms);
+            bw.Write(value);
+            var bytes = ms.ToArray();
+            ms.Dispose();
+            bw.Dispose();
+            SetBytes(key, bytes);
+        }
+
+        public int GetInt(string key)
+        {
+            return BitConverter.ToInt32(GetBytes(key));
+        }
+
+        public int GetInt(byte[] key)
+        {
+            return BitConverter.ToInt32(GetBytes(key));
+        }
+
+        public int GetShort(string key)
+        {
+            return BitConverter.ToInt16(GetBytes(key));
+        }
+
+        public int GetShort(byte[] key)
+        {
+            return BitConverter.ToInt16(GetBytes(key));
+        }
+
+        public int GetUShort(string key)
+        {
+            return BitConverter.ToUInt16(GetBytes(key));
+        }
+
+        public int GetUShort(byte[] key)
+        {
+            return BitConverter.ToUInt16(GetBytes(key));
+        }
+
+        public uint GetUInt(string key)
+        {
+            return BitConverter.ToUInt32(GetBytes(key));
+        }
+
+        public uint GetUInt(byte[] key)
+        {
+            return BitConverter.ToUInt32(GetBytes(key));
+        }
+
+        public long GetLong(string key)
+        {
+            return BitConverter.ToInt64(GetBytes(key));
+        }
+
+        public long GetLong(byte[] key)
+        {
+            return BitConverter.ToInt64(GetBytes(key));
+        }
+
+        public ulong GetULong(string key)
+        {
+            return BitConverter.ToUInt64(GetBytes(key));
+        }
+
+        public ulong GetULong(byte[] key)
+        {
+            return BitConverter.ToUInt64(GetBytes(key));
+        }
+
+        public string GetString(string key)
+        {
+            var bytes = GetBytes(key);
+            MemoryStream ms = new(bytes);
+            BinaryReader br = new(ms);
+            var str = br.ReadString();
+            ms.Dispose();
+            br.Dispose();
+            return str;
+        }
+
+        public string GetString(byte[] key)
+        {
+            var bytes = GetBytes(key);
+            MemoryStream ms = new(bytes);
+            BinaryReader br = new(ms);
+            var str = br.ReadString();
+            ms.Dispose();
+            br.Dispose();
+            return str;
+        }
+    }
+}
---
+++ AtomDB/AtomManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomDB
+{
+    public static class AtomManager
+    {
+        /// <summary>
+        /// Checks if there will be a performance increase in using multi threaded Atom.
+        /// </summary>
+        public static bool UseMultiThreaded
+        {
+            get;
+            private set;
+        }
+
+        internal static bool Initialized = false;
+
+        public static void Initialize()
+        {
+            Initialized = true;
+
+            Atom a = Atom.Create();
+            AtomMT b = AtomMT.Create();
+            Stopwatch sw = new();
+            sw.Start();
+            a.SetBytes(new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            var aCheckBytes = a.GetBytes(new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            sw.Stop();
+            var aMS = sw.ElapsedMilliseconds;
+            sw.Reset();
+            sw.Start();
+            b.SetBytes(new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            var bCheckBytes = b.GetBytes(new byte[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
+            sw.Stop();
+            var bMS = sw.ElapsedMilliseconds;
+            UseMultiThreaded = bMS < aMS;
+        }
+    }
+}
